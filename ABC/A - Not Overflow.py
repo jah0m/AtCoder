e64d22d883d74bd6f7c228dedabe3e9a7c6aa68c
jah0m <jahom990831@gmail.com>
2022-01-31 03:56:37 +0900

    220130

--- ABC/A - Not Overflow.py
+++ ABC/A - Not Overflow.py
@@ -1,10 +1,2 @@
-H, W = map(int,input().split())
-a = [[int(x) for x in input().split()] for _ in range(H)]
-b = [[0] * H for _ in range(W)]
-
-for i in range(H):
-    for j in range(W):
-        b[j][i] = a[i][j]
-
-for l in b:
-    print(*l)
+N = int(input())
+print('Yes' if -2**31<=N<2**31 else 'No')   
