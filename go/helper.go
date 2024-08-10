77f598a4fea753e53fc1d9c036a4c6d7231314e4
jah0m <jahom990831@gmail.com>
2024-08-11 00:22:44 +0900

    update helper.go

--- go/helper.go
+++ go/helper.go
@@ -28,15 +28,16 @@ func (i *Iost) Text() string {
 	}
 	return i.Scanner.Text()
 }
-func (i *Iost) Atoi(s string) int                 { x, _ := strconv.Atoi(s); return x }
-func (i *Iost) GetNextInt() int                   { return i.Atoi(i.Text()) }
-func (i *Iost) Atoi64(s string) int64             { x, _ := strconv.ParseInt(s, 10, 64); return x }
-func (i *Iost) GetNextInt64() int64               { return i.Atoi64(i.Text()) }
-func (i *Iost) Atof64(s string) float64           { x, _ := strconv.ParseFloat(s, 64); return x }
-func (i *Iost) GetNextFloat64() float64           { return i.Atof64(i.Text()) }
-func (i *Iost) Print(x ...interface{})            { fmt.Fprint(i.Writer, x...) }
-func (i *Iost) Printf(s string, x ...interface{}) { fmt.Fprintf(i.Writer, s, x...) }
-func (i *Iost) Println(x ...interface{})          { fmt.Fprintln(i.Writer, x...) }
+func (i *Iost) Atoi(s string) int         { x, _ := strconv.Atoi(s); return x }
+func (i *Iost) Itoa(x int) string         { return strconv.Itoa(x) }
+func (i *Iost) GetNextInt() int           { return i.Atoi(i.Text()) }
+func (i *Iost) Atoi64(s string) int64     { x, _ := strconv.ParseInt(s, 10, 64); return x }
+func (i *Iost) GetNextInt64() int64       { return i.Atoi64(i.Text()) }
+func (i *Iost) Atof64(s string) float64   { x, _ := strconv.ParseFloat(s, 64); return x }
+func (i *Iost) GetNextFloat64() float64   { return i.Atof64(i.Text()) }
+func (i *Iost) Print(x ...any)            { fmt.Fprint(i.Writer, x...) }
+func (i *Iost) Printf(s string, x ...any) { fmt.Fprintf(i.Writer, s, x...) }
+func (i *Iost) Println(x ...any)          { fmt.Fprintln(i.Writer, x...) }
 func max[T ~int | ~int64](a, b T) T {
 	if a < b {
 		return b
